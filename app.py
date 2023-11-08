51e8bbd925a2adffd1549792d722985e6cf08e57
Nikola_Dakov <nikoladakov438@gmail.com>
2023-11-08 21:31:10 +0100

    formating code

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+
 # Dictionary to store predefined user credentials
 user1 ={'Mario12': '12'}
 user2 ={'Luigi13': '13'}
+
 
 # Route for rendering the login form
 @app.route('/')
 def view_form():
     return render_template('loginStaff.html')  
+
 
 # Route to handle login 
 @app.route('/login', methods=['POST'])
@@ -24,9 +27,10 @@ def login():
             return redirect('/kitchenPage')  # Redirect to the 'luigiPage/kitchenPage' route
         else:
             return redirect('/invalidMessage')
+
+
 # -----------------------------------------------------------------------------------------------------------------------
 # this will be for trasfer data from menuPage to mario and luigi's pages
-# Create a list to store orders (you can replace this with a database)
 # Logic and Conecting
 orders = []
 total_price = 0
@@ -42,24 +46,32 @@ def place_order():
         
     }
     orders.append(order)
+
      # Format the orders list
     formatted_orders = []
     for order in orders:
         formatted_order = f'Pizza: {order["Name"]} Price: {order["Price"]} Euro'
         formatted_orders.append(formatted_order)
+
+
     # calculate total price
     global total_price
     total_price += item_price
 
     return render_template('MenuPage.html', formatted_orders=formatted_orders, total_price=total_price)
 # ---------------------------------------------------------------------------------------------------------------
+
 #redirect orders to luigi
 @app.route("/orderbutton", methods=['POST'])
 def orderbutton():
     return render_template("/marioPage.html", orders=orders, total_price=total_price)
+
+
 # -------------------------------------------------------------------------------------------------------------------5
 # -------------------------------------------------------------------------------------------------------------------5
+
 # Reset Function
+
 @app.route('/resetbutton', methods=['POST'])
 def resetbutton():
     orders.clear()
@@ -67,25 +79,31 @@ def resetbutton():
     total_price = 0
     return redirect('/MenuPage')
 # -----------------------------------------------------------------------------------------------------------------------
+
 # Route to display the 'marioPage.html'
 @app.route('/marioPage')
 def mario_page():
     return render_template('marioPage.html')  
+
+
 
 #route to display LuigiPage or KitchenPage
 @app.route('/kitchenPage')
 def kitchenPage():
     return render_template('kitchenPage.html', orders=orders) 
 
+
 # Route to display the 'invalidMessage.html'
 @app.route('/invalidMessage')
 def invalid_message():
     return render_template('invalidMessage.html')
 
+
 # Route  to display the menupage!
 @app.route('/MenuPage')
 def MenuPage():
     return render_template('MenuPage.html')  
+
 
 if __name__ == '__main__':
     app.run()
